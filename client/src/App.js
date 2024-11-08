8fd5612577f01581ba2ebf10db4341c9f86b8e4b
Danupa Sangeeth <139672209+DanupaSangeeth@users.noreply.github.com>
2024-11-08 15:27:41 +0530

    18th commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,3 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import AdminLogin from './AdminLogin';
-import AdminHome from './AdminHome';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -13,31 +8,24 @@ import SignIn from './components/SignIn/SignIn';
 import Home from './components/Home/Home';
 import SignUp from './components/SignUp/SignUp';
 
-const App = () => {
-    const isAuthenticated = () => {
-        const token = localStorage.getItem('token');
-        // In a real-world application, verify the token with the server for expiration, etc.
-        return !!token;
-    };
 
-    return (
-        <Router>
-            <Switch>
-                <Route path="/admin/login" component={AdminLogin} />
-                <Route path='/' element={<LandingPage/>}/>
-                <Route path='/home' element={<Home/>}/>
-                <Route path='/signin' element={<SignIn/>}/>
-                <Route path='/signup' element={<SignUp/>}/>
-                
-                <Route 
-                    path="/admin/home" 
-                    render={() => isAuthenticated() ? <AdminHome /> : <Redirect to="/admin/login" />} 
-                />
 
-                <Redirect from="/" to="/admin/login" />
-            </Switch>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+        <ToastContainer position="top-center" style={{marginTop: "70px"}}/>
+            <main>
+              <Routes>
+              <Route path='/' element={<LandingPage/>}/>
+              <Route path='/home' element={<Home/>}/>
+              <Route path='/signin' element={<SignIn/>}/>
+              <Route path='/signup' element={<SignUp/>}/>
+              </Routes>
+            </main>
+        </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
