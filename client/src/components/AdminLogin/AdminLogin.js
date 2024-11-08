fd41f8e4c16cca273c459d04b2406d26a36096c7
Danupa Sangeeth <139672209+DanupaSangeeth@users.noreply.github.com>
2024-11-08 19:12:57 +0530

    34 commit

--- client/src/components/AdminLogin/AdminLogin.js
+++ client/src/components/AdminLogin/AdminLogin.js
@@ -5,15 +5,14 @@ import './AdminLogin.css'; // Import the CSS file
 const AdminLogin = ({ setIsAdminAuthenticated }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-      // Make an API call to authenticate the admin
-      const response = await fetch('http://localhost:8086/admin-login', {  // Update the URL as needed
+      // Make an API call to authenticate admin
+      const response = await fetch('http://backend-for-hostted-server.vercel.app/admin-login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -27,14 +26,13 @@ const AdminLogin = ({ setIsAdminAuthenticated }) => {
         // On successful login, store the token and update the authentication state
         localStorage.setItem('adminToken', data.token);  // Store token in localStorage
         setIsAdminAuthenticated(true);
-        navigate('/admin/home');  // Redirect to Admin Home Page
+        navigate('/admin-home');  // Redirect to Admin Home Page
       } else {
-        // Show an error message if login fails
-        setError(data.message || 'Invalid credentials');
+        alert('Invalid credentials');
       }
-    } catch (err) {
-      console.error('Login error:', err);
-      setError('Something went wrong. Please try again later.');
+    } catch (error) {
+      console.error("Error during login:", error);
+      alert('Something went wrong, please try again later.');
     }
   };
 
@@ -42,25 +40,23 @@ const AdminLogin = ({ setIsAdminAuthenticated }) => {
     <div className="admin-login-container">
       <form className="admin-login-form" onSubmit={handleLogin}>
         <h2>Admin Login</h2>
-
+        
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
+        
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
+        
         <button type="submit">Login</button>
-
-        {error && <div className="error-message">{error}</div>}  {/* Show error message if there's one */}
-
+        
         <a href="/forgot-password" className="forgot-password">Forgot password?</a>
       </form>
     </div>
