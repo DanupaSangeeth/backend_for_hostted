5c4c27be29fb0ed5e73c38ca1a6c774216d1ff40
Danupa Sangeeth <139672209+DanupaSangeeth@users.noreply.github.com>
2024-11-09 00:37:18 +0530

    51th commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: '*',
+  origin: 'https://www.danupa.me', // Allow requests from your domain
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
