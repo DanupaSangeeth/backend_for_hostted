0450606461fc0ba65f2dc4a83229b01d8af7d4fd
Danupa Sangeeth <139672209+DanupaSangeeth@users.noreply.github.com>
2024-11-08 15:30:17 +0530

    19th commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
@@ -7,23 +7,54 @@ import LandingPage from './components/LandingPage/LandingPage';
 import SignIn from './components/SignIn/SignIn';
 import Home from './components/Home/Home';
 import SignUp from './components/SignUp/SignUp';
-
-
+import AdminLogin from './components/AdminLogin/AdminLogin'; // Admin login page
+import AdminHome from './components/AdminHome/AdminHome'; // Admin home page
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const [isAdminAuthenticated, setIsAdminAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  // Example to check if admin is authenticated (you can use JWT or session)
+  useEffect(() => {
+    const token = localStorage.getItem('adminToken');
+    if (token) {
+      setIsAdminAuthenticated(true);
+    }
+  }, []);
+
   return (
     <div className="App">
-        <BrowserRouter>
-        <ToastContainer position="top-center" style={{marginTop: "70px"}}/>
-            <main>
-              <Routes>
-              <Route path='/' element={<LandingPage/>}/>
-              <Route path='/home' element={<Home/>}/>
-              <Route path='/signin' element={<SignIn/>}/>
-              <Route path='/signup' element={<SignUp/>}/>
-              </Routes>
-            </main>
-        </BrowserRouter>
+      <BrowserRouter>
+        {/* Toast notifications */}
+        <ToastContainer position="top-center" style={{ marginTop: '70px' }} />
+        
+        {/* Main content */}
+        <main>
+          <Routes>
+            {/* Landing Page */}
+            <Route path="/" element={<LandingPage />} />
+
+            {/* Home page */}
+            <Route path="/home" element={<Home />} />
+
+            {/* Sign-in page */}
+            <Route path="/signin" element={<SignIn />} />
+
+            {/* Sign-up page */}
+            <Route path="/signup" element={<SignUp />} />
+
+            {/* Admin Login Page */}
+            <Route path="/admin/login" element={<AdminLogin setIsAdminAuthenticated={setIsAdminAuthenticated} />} />
+
+            {/* Protected Admin Home Page */}
+            <Route
+              path="/admin/home"
+              element={isAdminAuthenticated ? <AdminHome /> : <SignIn />} // Only show Admin Home if authenticated
+            />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
